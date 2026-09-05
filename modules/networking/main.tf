d7d63ff62bf24a1c93c70a17e7538b0147d5bbc7
juparefe <jp_rendon93@hotmail.com>
2026-09-05 15:10:41 -0500

    Add nat_gateway

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -65,6 +65,11 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
   tags = {
     Name = "${var.project_name}-${var.environment}-private-rt"
   }
@@ -75,4 +80,23 @@ resource "aws_route_table_association" "private" {
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  depends_on = [aws_internet_gateway.this]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat"
+  }
 }
