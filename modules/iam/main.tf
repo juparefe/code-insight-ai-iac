c98b5f262572bc7993776412409d96ff7e1636cc
juparefe <jp_rendon93@hotmail.com>
2026-09-06 21:54:10 -0500

    Add oidc and update readme

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -104,3 +104,56 @@ resource "aws_iam_role_policy" "dynamodb_analysis_jobs" {
     ]
   })
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-github-oidc"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role" "github_actions_frontend" {
+  name = "${var.project_name}-${var.environment}-github-actions-frontend"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:juparefe/code-insight-ai-frontend:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-github-actions-frontend"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
